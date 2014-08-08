ee3b479e63f598225d5f4afc94f9060571aaf414
icedream <icedream2k9@die-optimisten.net>
2014-08-08 22:27:00 +0200

    Fix mono installation script.

--- .ci/install-mono.sh
+++ .ci/install-mono.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -eux
+#!/bin/sh -ux
 MONO_VER_PREFIX=""
 if [ "${MONO_VER}" == "2.10.8_3" ]; then
 	MONO_VER_PREFIX="3/"
@@ -14,7 +14,7 @@ wget -Omono.pkg "http://download.xamarin.com/MonoFrameworkMDK/Macx86/MonoFramewo
 	wget -Omono.dmg "http://download.mono-project.com/archive/${MONO_VER}/${MONO_VER_PREFIX}macos-10-x86/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.dmg" || \
 
 if [ -e "mono.dmg" ]; then
-	hdid "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.dmg"
+	hdid "mono.dmg"
 	sudo installer -pkg "/Volumes/Mono Framework MDK ${MONO_VER}/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
 else
 	sudo installer -pkg "mono.pkg" -target /
