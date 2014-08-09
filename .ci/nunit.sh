c7c816707ac2587d9d27f0aee01acb977fc922ea
icedream <icedream2k9@die-optimisten.net>
2014-08-09 03:26:27 +0200

    Be less verbose and automatically uses latest NUnit.Runners.

--- .ci/nunit.sh
+++ .ci/nunit.sh
@@ -1,9 +1,9 @@
-#!/bin/sh -x
+#!/bin/sh
 
-mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.3 -o packages
+mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -o packages
 
 runTest(){
-   mono --runtime=v4.0 packages/NUnit.Runners.2.6.3/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
+   mono --runtime=v4.0 packages/NUnit.Runners.*/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
    if [ $? -ne 0 ]
    then
      exit 1
