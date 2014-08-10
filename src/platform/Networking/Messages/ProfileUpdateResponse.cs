69282294f3e414b4c93fdca6002b99c8f90f8998
icedream <icedream2k9@die-optimisten.net>
2014-08-10 09:51:12 +0200

    Implement ProfileUpdate message, see #1.

--- src/platform/Networking/Messages/ProfileUpdateResponse.cs
+++ src/platform/Networking/Messages/ProfileUpdateResponse.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace DreamNetwork.PlatformServer.Networking.Messages
 {
     [Message(3u << 16 | 1u, MessageDirection.ToClient)]
@@ -5,5 +8,12 @@ namespace DreamNetwork.PlatformServer.Networking.Messages
     {
         public bool Success { get; set; }
         public string[] FailedFields { get; set; }
+    }
+
+    [Message(3u << 16 | 0u, MessageDirection.ToClient)]
+    public class ProfileUpdate : Message
+    {
+        public Guid ClientGuid { get; set; }
+        public Dictionary<string, object> ProfileFields { get; set; } 
     }
 }
