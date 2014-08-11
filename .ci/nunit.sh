c61f64e3d7c909370125e0ee0c6fc07fb434e9ad
icedream <icedream2k9@die-optimisten.net>
2014-08-11 06:11:50 +0200

    Attempt on circumventing known assert/crashing bug in Mono 3.0.x and 2.x with NUnit.

--- .ci/nunit.sh
+++ .ci/nunit.sh
@@ -3,7 +3,18 @@
 mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -o packages
 
 runTest(){
-   mono --runtime=v4.0 packages/NUnit.Runners.*/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
+	# check mono version to avoid specific bugs
+	monoVersion=`mono --version | head -n1 | cut -f1 -d"(" | sed 's/[[:alpha:]|(|[:space:]]//g' | awk -F- '{print $1}'`
+	monoArgs="--runtime=v4.0"
+	case "$monoVersion" in
+		2.*)
+		3.0.*)
+			echo "Disabling gshared optimization to circumvent mono bugs."
+			monoArgs="$monoArgs -O=-gshared"
+		;;
+	esac
+
+   mono $monoArgs packages/NUnit.Runners.*/tools/nunit-console.exe -noxml -nodots -labels $@
    if [ $? -ne 0 ]
    then
      exit 1
